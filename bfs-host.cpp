7a976b1c2d0def5663a263d68661323d64c79176
Christopher Liu <chrisliu@cs.ucla.edu>
2021-12-02 18:46:28 +0000

    [core, bug] added validation code and fixed pull bug.

--- bfs-host.cpp
+++ bfs-host.cpp
@@ -13,8 +13,9 @@
 #include "bfs-cpu.h"
 #include "util.h"
 
-constexpr nid_t PRINT_MAX_NODES = 10;
-constexpr offset_t PRINT_MAX_EDGES = 30;
+constexpr nid_t    PRINT_MAX_NODES  = 10;
+constexpr offset_t PRINT_MAX_EDGES  = 30;
+constexpr nid_t    PRINT_MAX_ERRORS = 10;
 
 int main(int argc, char *argv[]) {
   // Load graph.
@@ -79,58 +80,67 @@ int main(int argc, char *argv[]) {
     });
   }
 
-  std::vector<depth_t> depth(pullG.num_nodes, INVALID_DEPTH);
-
-  std::string bitstream;
-  if (const auto bitstream_ptr = getenv("TAPAB")) {
-    bitstream = bitstream_ptr;
-  }
-
-  tapa::invoke(
-      bfs_fpga, bitstream, 0, pushG.num_nodes,
-      tapa::read_only_mmap<offset_t>(pushG.index),
-      tapa::read_only_mmap<nid_t>(pushG.neighbors),
-      tapa::read_only_mmap<offset_t>(pullG.index),
-      tapa::read_only_mmap<nid_t>(pushG.neighbors),
-      tapa::read_write_mmap<depth_t>(depth));
-
   DEBUG(
-  if (pushG.num_nodes <= PRINT_MAX_NODES) {
-    for (nid_t u = 0; u < pushG.num_nodes; u++)
-      std::cout << depth[u] << " ";
+  std::cout << "Pull Graph" << std::endl
+            << "- num nodes: " << pullG.num_nodes << std::endl
+            << "- num edges: " << pullG.num_edges << std::endl;
+  for (nid_t u = 0; u < 10; u++) {
+    std::cout << "- " << u << ": ";
+    for (offset_t off = pullG.index[u]; off < pullG.index[u + 1]; off++) {
+      std::cout << pullG.neighbors[off] << " ";
+    }
     std::cout << std::endl;
-  });
+  }
+  );
 
-  //// Validate depths.
-  //{
-    //// Get validation depths for push.
-    //{
-      //std::vector<depth_t> depths(pushG.num_nodes, INVALID_DEPTH);
-      //nid_t start = 0; // Arbitrary (needs to be random in the future).
-      //bfs_cpu_push(pushG, start, depths);
-      
-      //DEBUG(
-      //if (pushG.num_nodes <= PRINT_MAX_NODES) {
-        //for (nid_t u = 0; u < pushG.num_nodes; u++)
-          //std::cout << depths[u] << " ";
-        //std::cout << std::endl;
-      //});
-    //}
 
-    //// Get validation depths for pull.
-    //{
-      //std::vector<depth_t> depths(pushG.num_nodes, INVALID_DEPTH);
-      //nid_t start = 0; // Arbitrary (needs to be random in the future).
-      //bfs_cpu_pull(pullG, start, depths);
-      
-      //DEBUG(
-      //if (pullG.num_nodes <= PRINT_MAX_NODES) {
-        //for (nid_t u = 0; u < pullG.num_nodes; u++)
-          //std::cout << depths[u] << " ";
-        //std::cout << std::endl;
-      //});
-    //}
-  //}
+  // Run and validate FPGA kernel.
+  {
+    nid_t start = 0; // Arbitrary.
+    std::vector<depth_t> fpga_depths(pullG.num_nodes, INVALID_DEPTH);
+
+    std::string bitstream;
+    if (const auto bitstream_ptr = getenv("TAPAB")) {
+      bitstream = bitstream_ptr;
+    }
+
+    tapa::invoke(
+        bfs_fpga, bitstream, 0, pushG.num_nodes,
+        tapa::read_only_mmap<offset_t>(pushG.index),
+        tapa::read_only_mmap<nid_t>(pushG.neighbors),
+        tapa::read_only_mmap<offset_t>(pullG.index),
+        tapa::read_only_mmap<nid_t>(pullG.neighbors),
+        tapa::read_write_mmap<depth_t>(fpga_depths));
+
+    DEBUG(
+    if (pushG.num_nodes <= PRINT_MAX_NODES) {
+      for (nid_t u = 0; u < pushG.num_nodes; u++)
+        std::cout << fpga_depths[u] << " ";
+      std::cout << std::endl;
+    });
+
+    std::vector<depth_t> validation_depths(pushG.num_nodes, INVALID_DEPTH);
+    bfs_cpu_push(pushG, start, validation_depths);
+
+    nid_t err_count = 0;
+    for (nid_t u = 0; u < pushG.num_nodes; u++) {
+      if (fpga_depths[u] != validation_depths[u]) {
+        if (err_count < PRINT_MAX_ERRORS) {
+          DEBUG(std::cout << "[error] node " << u << " fpga depth (" 
+                          << fpga_depths[u] << ") != oracle depth ("
+                          << validation_depths[u] << ")" << std::endl);
+        }
+        err_count++;
+      }
+    }
+    if (err_count >= PRINT_MAX_ERRORS) {
+      DEBUG(std::cout << "[error] ... " << std::endl
+                      << "[error] and " << (err_count - PRINT_MAX_ERRORS)
+                      << " more errors" << std::endl);
+    }
+    if (err_count != 0) return EXIT_FAILURE;
+    else /* Success */  DEBUG(std::cout << "Validation success!" << std::endl);
+  }
 
   return EXIT_SUCCESS;
 }
