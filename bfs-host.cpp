af8e0cdb73d73de4fe143ccd88b57860b078df09
Christopher Liu <chrisliu@cs.ucla.edu>
2021-12-11 23:31:57 +0000

    [core] added graph partitioning based on edge count.

--- bfs-host.cpp
+++ bfs-host.cpp
@@ -1,5 +1,8 @@
 #define DEBUG_ON
 #define VERTEX_CENTRIC
+
+constexpr int NUM_PARTITIONS = 2;
+
 #include <algorithm>
 #include <cstdint>
 #include <iostream>
@@ -90,16 +93,22 @@ int main(int argc, char *argv[]) {
       bitstream = bitstream_ptr;
     }
 
-    // Resize push neighbors vector to align properly.
-    size_t new_size = (pushG.neighbors.size() + NEIGHBORS_CHUNK_SIZE - 1)
-                        / NEIGHBORS_CHUNK_SIZE * NEIGHBORS_CHUNK_SIZE;
-    pushG.neighbors.resize(new_size);
+    auto partition = partition_edges(&pushG, V_NUM_PARTITIONS);
+    auto index_es = std::get<0>(partition);
+    auto neighbors_es = std::get<1>(partition);
+    auto num_nodes = std::get<2>(partition);
+
+    DEBUG(
+    for (int i = 0; i < V_NUM_PARTITIONS; i++) {
+      std::cout << "Partition " << i << ": " << num_nodes[i] << " nodes and "
+                << neighbors_es[i].size() << " edges" << std::endl;
+    });
 
     tapa::invoke(
         bfs_fpga, bitstream, 
         start_nid, pushG.num_nodes, 
         tapa::read_only_mmap<offset_t>(pushG.index),
-        tapa::read_only_mmap<nid_t>(pushG.neighbors).reinterpret<bits<nid_vec_t>>(),
+        tapa::read_only_mmap<nid_t>(pushG.neighbors),
         tapa::read_write_mmap<depth_t>(fpga_depths));
 
     DEBUG(
--- graph.h
+++ graph.h
@@ -3,17 +3,21 @@
 
 #include <cstdint>
 #include <istream>
+#include <tuple>
 #include <vector>
 // For edge-centric
 #include <tapa.h>
 #include <ap_int.h>
 
 // Base types.
-using nid_t       = int32_t;
-using offset_t    = nid_t;
-using edge_t      = std::pair<nid_t, nid_t>;
-using edge_list_t = std::vector<edge_t>;
-using depth_t     = int;
+using nid_t        = int32_t;
+using offset_t     = nid_t;
+using edge_t       = std::pair<nid_t, nid_t>;
+using edge_list_t  = std::vector<edge_t>;
+using depth_t      = int;
+using nid_vec_t    = std::vector<nid_t>;
+using offset_vec_t = std::vector<offset_t>;
+
 // Base types for Edge-centric
 const int PARTITION_NUM = 2; //should be sizeof(BRAM)/sizeof(edge+vertex)
 const int MAX_EDGE = 1024;
@@ -57,10 +61,10 @@ constexpr depth_t INVALID_DEPTH = -1;
  *    v = neighbors[off]
  */
 struct CompressedGraph {
-  std::vector<offset_t> index;     // offset array
-  std::vector<nid_t>    neighbors; // edge array 
-  nid_t                 num_nodes;
-  offset_t              num_edges;
+  offset_vec_t index;     // offset array
+  nid_vec_t    neighbors; // edge array 
+  nid_t        num_nodes;
+  offset_t     num_edges;
 };
 
 // Compressed graph formats.
@@ -72,7 +76,6 @@ using PullGraph = CompressedGraph;
  */
 edge_list_t load_edgelist(std::istream &in);
 
-
 /**
  * Constructs CSR and CSC graphs from an edge list.
  * Parmeters:
@@ -82,6 +85,9 @@ edge_list_t load_edgelist(std::istream &in);
  */
 void build_graphs(edge_list_t &edge_list, 
     PushGraph * const pushG, PullGraph * const pullG);
+
+std::tuple<std::vector<offset_vec_t>, std::vector<nid_vec_t>, std::vector<nid_t>>
+partition_edges(PushGraph * const g, int num_partitions);
 
 // Sort functions.
 struct {
